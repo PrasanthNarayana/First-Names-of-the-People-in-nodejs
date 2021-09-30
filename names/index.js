a180086d85d720748952da40ab2409844d2f2145
PrasanthNarayana <prasanthnarayana522@gmail.com>
2021-09-30 16:14:20 +0000

    First Names of the People

---
+++ names/index.js
@@ -0,0 +1,7 @@
+const teamMemberNames = require("../country/state/city/index");
+const getFirstNames = require("../utilities/utils/index");
+
+function getPeopleInCity(teamMemberNames) {
+  return getFirstNames(teamMemberNames);
+}
+module.exports = getPeopleInCity;
